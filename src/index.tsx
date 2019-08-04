dab510e7621c92c7f1c11331fb315e0c806d79f8
ahuglajbclajep <ahuglajbclajep@gmail.com>
2019-08-05 00:57:44 +0900

    format by prettier

--- src/index.tsx
+++ src/index.tsx
@@ -7,16 +7,28 @@ import "./style.css";
 
 type Props = { worker: ComlinkClass<WorkerAPI> };
 const App: FunctionComponent<Props> = ({ worker }) => {
+  const [markdown, setMarkdown] = useState("");
   const [html, setHtml] = useState("");
 
   const onInput: h.JSX.GenericEventHandler = async e => {
-    setHtml(await worker.md2html((e.target as HTMLTextAreaElement).value));
+    const md = (e.target as HTMLTextAreaElement).value;
+    setMarkdown(md);
+    setHtml(await worker.md2html(md));
+  };
+
+  const onKeyDown: h.JSX.KeyboardEventHandler = async e => {
+    if (e.ctrlKey && e.key === "s") {
+      e.preventDefault();
+      setMarkdown(await worker.format(markdown));
+    }
   };
 
   return (
-    <div className="container">
+    // see https://stackoverflow.com/questions/43503964/onkeydown-event-not-working-on-divs-in-react
+    <div className="container" tabIndex={0} onKeyDown={onKeyDown}>
       <textarea
         className="edit-area"
+        value={markdown}
         onInput={onInput}
         autoFocus
         placeholder="# mdpreview"
--- src/worker.ts
+++ src/worker.ts
@@ -4,6 +4,9 @@ import mutate from "remark-rehype";
 import stringify from "rehype-stringify";
 import { expose } from "comlink";
 
+let format: typeof import("prettier/standalone").format | undefined;
+let mdPlugin: typeof import("prettier/parser-markdown") | undefined;
+
 class WebWorker {
   md2html(markdown: string): string {
     return unified()
@@ -13,8 +16,23 @@ class WebWorker {
       .processSync(markdown)
       .toString();
   }
+
+  format(markdown: string): string {
+    return format && mdPlugin
+      ? format(markdown, { parser: "markdown", plugins: [mdPlugin] })
+      : markdown;
+  }
 }
 
 expose(WebWorker);
+
+(async () => {
+  const [formatter, plugin] = await Promise.all([
+    import("prettier/standalone"),
+    import("prettier/parser-markdown")
+  ]);
+  format = formatter.format;
+  mdPlugin = plugin;
+})();
 
 export type WorkerAPI = typeof WebWorker;
